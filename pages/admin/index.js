5316d1e28d9b70a3c8cbc1a123731e15379c5b81
Gabriel Raskov <gabras0528@skynet.halmstad.se>
2022-03-08 09:52:08 +0100

    Lagt till

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -8,6 +8,8 @@ import ConfirmedBooking from "../../models/ConfirmedBooking"
 
 export async function getStaticProps(context) {
     dbConnect()
+
+    //Create Response
     let response = undefined
         try {
             let AvailableBookings = await AvailableBooking.find({})
@@ -25,7 +27,41 @@ export async function getStaticProps(context) {
     await console.log(JSON.stringify(response))
 
     const data = {}
-    await fetch('http://localhost:3000/api/checkbookings')
+
+    // Check bookings
+
+        console.log("Checking bookings!")
+        let AvailableBookings = await AvailableBooking.find({})
+        let ConfirmedBookings = await ConfirmedBooking.find({})
+
+        for (let i=0; i < AvailableBooking.length; i++) {
+            try{
+                if(Date.parse(AvailableBookings[i].date)-Date.parse(new Date()) <= 0) {
+                    console.log("Trying to delete date")
+                    await AvailableBooking.findOneAndDelete({date: AvailableBookings[i].date})
+                }
+            }
+            catch {
+
+            }
+        }
+        
+        for (let i=0; i < ConfirmedBookings.length; i++) {
+            let day = ConfirmedBookings[i].date.split("-")
+            if(parseInt(day[2]) < new Date().getDate()) {
+                console.log("Removing confirmed booking")
+                await ConfirmedBooking.findOneAndDelete({date: ConfirmedBookings[i].date}, (err) => {
+                    console.log(err)
+                })
+            }
+        
+            
+        }
+
+        console.log("From, admin-get : Fetched Available bookings")
+        response = {AvailableBookings: AvailableBookings, ConfirmedBookings: ConfirmedBookings}
+    
+    //Return props
     return {props: {data: data}}
 }
 
