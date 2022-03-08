a9aa5d22e699ad6f9b69e4c6e698f0937626a14f
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2022-03-08 22:11:30 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -136,6 +136,7 @@ catch(error) {
 
 export default function Home({data, information, pictures}) {
     console.log("pictures array : " + pictures)
+    pictures = JSON.parse(pictures)
     let AvailableBookings = data.AvailableBookings
     let sortedBookings = AvailableBookings.sort((a,b) => {
     try {
