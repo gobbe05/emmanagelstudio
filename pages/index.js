c49eee4ffe947aea5d28a10625e51804bceb167e
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2022-03-08 20:27:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,6 @@ catch(error) {
     let pictures = picturesRes
     
     if(information == undefined) {information = null}
-    if(pictures == undefined) {pictures = []}
   
   //Check bookings
         console.log("Checking bookings!")
