afef8768c16d2aa29d61f8beece748d201853d6f
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2022-03-09 10:35:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -148,8 +148,8 @@ export default function Home({data, information}) {
                 let image = "https://drive.google.com/uc?export=view&id=" + id
                 let object = {image: image, title: title, comment: comment}
                 if(name.split(".")[1]) {
-                    setImageArray(state => {return state.push(object)})
-                  console.log("Adding object")
+                    setImageArray(oldArray => [...oldArray,object])
+                    console.log("Image array : " + imageArray)
                 }
                 else {
                     console.log("File is not an image, skipping!")
