4d9628c9720185e19a8001cd1b083409fea3fe3b
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2022-03-09 10:48:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export async function getStaticProps(context) {
   //Admin call
   
   try {
-           let AvailableBookings = await AvailableBooking.find({})
+            let AvailableBookings = await AvailableBooking.find({})
             let ConfirmedBookings = await ConfirmedBooking.find({})
             console.log("From, admin-get : Fetched Available bookings")
             dataRes = {AvailableBookings: JSON.parse(JSON.stringify(AvailableBookings)), ConfirmedBookings: JSON.parse(JSON.stringify(ConfirmedBookings))}
