8d12c05f12df954773c17a8076079d5ccdd31d29
Gabriel Raskov <gabriel@raskov.se>
2022-03-14 21:00:34 +0100

    trying to fix safari 2

--- next.config.js
+++ next.config.js
@@ -4,30 +4,8 @@ const nextConfig = {
   },
 }
 
-module.exports = (phase, {defaultConfig}) => {
-  return {
-    ...defaultConfig,
-  webpack: (config) => {
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        "fs": false,
-         "path": false,
-         "os": false,
-         "crypto": false,
-         "http": false,
-         "http2": false,
-         "https": false,
-         "zlib": false,
-         "net": false,
-         "path": false,
-         "stream": false,
-         "querystring": false,
-         "child_process": false,
-         "tls": false
-      }
-    }
-    return config
-    },
-  }
+module.exports = {
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 }
